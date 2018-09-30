cb63dec632975150643efeb78b0ed64cbe08a30c
raopei <raopei1994@163.com>
2018-09-30 16:24:30 +0800

    class Man inherits from class Human.

--- main.cpp
+++ main.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 using namespace std;
 
-class human
+class Human
 {
 
 public:
-     human();//overload the constructor.
-     human(int age_tem, int hei_tem, string sex_tem);
-     ~human();//overload the destructor.
+     Human();//overload the constructor.
+     Human(int age_tem, int hei_tem, string sex_tem);
+     ~Human();//overload the destructor.
     void setAge(int a)
     {
         age = a;
@@ -40,37 +40,58 @@ private:
     string sex;
 
 };
+class Man :public Human
+{
+public:
+    void hair()
+    {
+        this->setSex("man");
+        cout<<"Man has short hair."<<endl;
+    }
+};
 
-    human::human(void)
+    Human::Human(void)
     {
         cout<<"An object of human has created."<<endl;
     }
-    human::human(int age_tem, int hei_tem, string sex_tem):age(age_tem),height(hei_tem),sex(sex_tem)
+    Human::Human(int age_tem, int hei_tem, string sex_tem):age(age_tem),height(hei_tem),sex(sex_tem)
     {
         cout<<"An object of human has created. age is "<<age<<endl;
     }
 
-    human::~human(void)
+    Human::~Human(void)
     {
         cout<<"The object has been destroyed."<<endl;
     }
 int main()
 {  
-    human m1;
+    Human m1;
     m1.setAge(20);
     m1.setHeight(180);
     m1.setSex("man");
 
-    human w1(18,170,"women");
+    Human w1(18,170,"women");
     //w1.setAge(18);
    // w1.setHeight(170);
     //w1.setSex("women");
 
+    Man m;
+    m.setAge(24);
+    m.setHeight(170);
+    m.hair();
+
+
     cout<<"m1:"<<endl<<m1.getAge()<<" years old"
                <<endl<<m1.getHeight()<<" tall"
                <<endl<<m1.getSex()<<endl;
+
     cout<<"w1:"<<endl<<w1.getAge()<<" years old"
                <<endl<<w1.getHeight()<<" tall"
                <<endl<<w1.getSex()<<endl;
+
+    cout<<"w1:"<<endl<<m.getAge()<<" years old"
+               <<endl<<m.getHeight()<<" tall"
+               <<endl<<m.getSex()<<endl;
+
     return 0;
 }
